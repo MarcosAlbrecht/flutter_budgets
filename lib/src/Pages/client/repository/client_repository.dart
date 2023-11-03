59dd68dfedf8c1668af185cefec35f0fc49b42e6
MarcosAlbrecht <marquinhos.beto@hotmail.com>
2023-11-03 16:41:20 -0300

    create client insert

--- lib/src/Pages/client/repository/client_repository.dart
+++ lib/src/Pages/client/repository/client_repository.dart
@@ -13,7 +13,7 @@ class ClientRepository {
     try {
       final userQuery = FirebaseFirestore.instance
           .collection('users')
-          .where('id_user', isEqualTo: 'pU5C8w4JXxMKGdN7Gy2U');
+          .where('id_user', isEqualTo: userID);
 
       final userCollectionReference = await userQuery.get();
 
@@ -23,17 +23,58 @@ class ClientRepository {
         final clientsCollectionReference =
             userDoc.reference.collection('clients');
 
-        final clientIDToUpdate = clientToUpdate.idClient;
-
         // Obtenha a referência para o documento do cliente que você deseja atualizar
-        final clientDocReference =
-            clientsCollectionReference.doc(clientIDToUpdate);
+        final clientDocReference = clientsCollectionReference
+            .where('id_client', isEqualTo: clientToUpdate.idClient)
+            .limit(1); // Certifique-se de limitar a 1, pois o ID deve ser único
+
+        final querySnapshot = await clientDocReference.get();
+
+        if (querySnapshot.docs.isNotEmpty) {
+          // Recupere o documento do cliente encontrado
+          final clientDoc = querySnapshot.docs.first;
+
+          // Crie um novo mapa de dados com as atualizações que deseja aplicar
+          final updatedData = clientToUpdate.toJson();
+
+          // Atualize o documento do cliente
+          await clientDoc.reference.update(updatedData);
+
+          return ClientResult.success('Cliente atualizado com sucesso');
+        } else {
+          return ClientResult.error(
+              'Nenhum cliente encontrado com o id_client especificado.');
+        }
+      } else {
+        return ClientResult.error(
+            'Nenhum usuário encontrado com o id_user especificado.');
+      }
+    } catch (e) {
+      print('Erro ao atualizar o cliente: $e');
+      return ClientResult.error('Erro na atualização: $e');
+    }
+  }
+
+  Future<ClientResult> insertClient({
+    required String userID,
+    required ClientModel clientToUpdate,
+  }) async {
+    try {
+      final userQuery = FirebaseFirestore.instance
+          .collection('users')
+          .where('id_user', isEqualTo: userID);
+
+      final userCollectionReference = await userQuery.get();
+
+      if (userCollectionReference.docs.isNotEmpty) {
+        final userDoc = userCollectionReference.docs.first;
 
         // Crie um novo mapa de dados com as atualizações que deseja aplicar
         final updatedData = clientToUpdate.toJson();
 
-        // Atualize o documento do cliente
-        await clientDocReference.update(updatedData);
+        // adiciona novo cliente
+        final clientsCollectionReference =
+            userDoc.reference.collection('clients').add(updatedData);
 
         return ClientResult.success('Cliente atualizado com sucesso');
       } else {
